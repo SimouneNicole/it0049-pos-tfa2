918d775aa3602f77ec2f5779c8f1e769d009443a
SimouneNicole <moonkirumi@gmail.com>
2026-09-18 18:05:52 +0800

    feat: implement CustomerModel and UserModel using CodeIgniter Model

---
+++ app/Models/CustomerModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CustomerModel extends Model
+{
+    protected $table            = 'customers';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'full_name',
+        'email',
+        'phone',
+        'created_at',
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+}
---
+++ app/Models/UserModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'username',
+        'full_name',
+        'created_at',
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+}
